256d500ed0d44dba6e23279e51ff8e491e4aebad
Piotr Bednarek <specjunior@gmail.com>
2022-10-06 17:48:20 +0200

    SetInterval function

--- lab1/circle.js
+++ lab1/circle.js
@@ -5,3 +5,10 @@ ctx.beginPath();
 ctx.arc(400, 300, 250, 0, 2 * Math.PI);
 ctx.fillStyle = "blue";
 ctx.fill();
+ctx.closePath();
+
+setInterval(ChangeColor, 1000);
+
+function ChangeColor(){
+    ctx.fillStyle==="blue"?"green":"blue";
+}
