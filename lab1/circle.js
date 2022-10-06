768d0ca717b975204e2c795c261ae81d23a582f6
Piotr Bednarek <specjunior@gmail.com>
2022-10-06 17:41:47 +0200

    Color circle

--- lab1/circle.js
+++ lab1/circle.js
@@ -2,6 +2,6 @@ var c = document.getElementById("circle");
 var ctx = c.getContext("2d");
 
 ctx.beginPath();
-ctx.fillStyle = "#FF0000";
 ctx.arc(400, 300, 250, 0, 2 * Math.PI);
-ctx.stroke();
+ctx.fillStyle = "blue";
+ctx.fill();
