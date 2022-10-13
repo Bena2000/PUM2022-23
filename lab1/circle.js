c35b0f1a663459d9d19a179fa2cce30b159a80ef
Piotr Bednarek <specjunior@gmail.com>
2022-10-13 15:55:51 +0200

    new ChangeColor function

--- lab1/circle.js
+++ lab1/circle.js
@@ -8,19 +8,12 @@ ctx.fill();
 ctx.closePath();
 
 setInterval(ChangeColor, 1000);
+var i=0;
 
-const element = document.getElementById("demo");
 function ChangeColor(){
-    if(ctx.fillStyle==="#0000ff")
-    {
-        ctx.fillStyle="green";
-    }
-    else
-    {
-        ctx.fillStyle="blue";
-    }
 
-    // ctx.fillStyle==="blue"?"green":"blue";
+    ctx.fillStyle= i===0?"green":"blue";
+    i=(i+1)%2;
     ctx.fill();
 }
 	
