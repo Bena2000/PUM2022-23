109a253610bdb00ac88a68ddf6e4059c57ee06c9
Piotr Bednarek <specjunior@gmail.com>
2022-10-13 17:01:27 +0200

    change color gradient

--- lab1b/circle.js
+++ lab1b/circle.js
@@ -1,31 +1,37 @@
 var c = document.getElementById("circle");
 var ctx = c.getContext("2d");
 //gradient
-var grd = ctx.createLinearGradient(0, 0, 500, 0);
-grd.addColorStop(0, "blue");
-grd.addColorStop(1, "green");
+// var grd = ctx.createLinearGradient(0, 0, 500, 0);
+// grd.addColorStop(0, "blue");
+// grd.addColorStop(1, "green");
 
 //shape code
 ctx.beginPath();
 ctx.arc(400, 300, 250, 0, 2 * Math.PI);
-ctx.fillStyle = grd;
+ctx.fillStyle = "blue";
 ctx.fill();
 ctx.closePath();
 
-for(let green=0;green<255;green++)
-{
-    ctx.fillStyle= 'rgb(0, green, 255-green)';
-    ctx.fill();
-}
-//0,green,255-green
-
-
-// setInterval(ChangeColor, 1000);
-var i=0;
+setInterval(ChangeColor, 1);
+var green=0;
+var changeToGreen=1;
 
 function ChangeColor(){
-
-    ctx.fillStyle= i===0?"green":"blue";
-    i=(i+1)%2;
+    if(changeToGreen===1)
+    {
+        green++;
+    }else{
+        green--;
+    }
+        
+    console.log(green);
+    ctx.fillStyle= `rgb(0, ${green}, ${255-green})`;
     ctx.fill();
+
+    if(green<=0)
+    {
+        changeToGreen=1;
+    }else if(green>=255){
+        changeToGreen=0;
+    }
 }
