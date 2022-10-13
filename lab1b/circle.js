bc8662d5aca779f1865667e22f5d1b386bf1dd97
Piotr Bednarek <specjunior@gmail.com>
2022-10-13 16:43:03 +0200

    added gradient

--- lab1b/circle.js
+++ lab1b/circle.js
@@ -1,13 +1,20 @@
 var c = document.getElementById("circle");
 var ctx = c.getContext("2d");
+//gradient
+var grd = ctx.createLinearGradient(0, 0, 450, 0);
+grd.addColorStop(0, "blue");
+grd.addColorStop(1, "green");
 
+//shape code
 ctx.beginPath();
 ctx.arc(400, 300, 250, 0, 2 * Math.PI);
-ctx.fillStyle = "blue";
+ctx.fillStyle = grd;
 ctx.fill();
 ctx.closePath();
+//0,green,250-green
 
-setInterval(ChangeColor, 1000);
+
+// setInterval(ChangeColor, 1000);
 var i=0;
 
 function ChangeColor(){
@@ -16,4 +23,3 @@ function ChangeColor(){
     i=(i+1)%2;
     ctx.fill();
 }
-	
