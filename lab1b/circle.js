b83f19fd1eafc62cc038da3b560a9a04eb302801
Piotr Bednarek <specjunior@gmail.com>
2022-10-13 17:09:26 +0200

    removed commented

--- lab1b/circle.js
+++ lab1b/circle.js
@@ -1,10 +1,5 @@
 var c = document.getElementById("circle");
 var ctx = c.getContext("2d");
-//gradient
-// var grd = ctx.createLinearGradient(0, 0, 500, 0);
-// grd.addColorStop(0, "blue");
-// grd.addColorStop(1, "green");
-
 //shape code
 ctx.beginPath();
 ctx.arc(400, 300, 250, 0, 2 * Math.PI);
