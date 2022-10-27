7d14babad44f649e727ea8cbf077d121d3fd0a18
Bena2000 <specjunior@gmail.com>
2022-10-27 17:04:41 +0200

    draw platform

--- lab3/move.js
+++ lab3/move.js
@@ -5,10 +5,25 @@ var x = c.width/2;
 var y = c.height-30;
 var dx = 2;
 var dy = -2;
+let rightPressed = false;
+let leftPressed = false;
+
+var platformHeight = 10;
+var platformWidth = 75;
+var platformX = (c.width-platformWidth)/2;
+var keyboardMoveSpeed=2;
 
 function drawBall() {
     ctxGradient.beginPath();
     ctxGradient.arc(x, y, 10, 0, Math.PI*2);
+    ctxGradient.fillStyle = "#0095DD";
+    ctxGradient.fill();
+    ctxGradient.closePath();
+}
+
+function drawPlayer() {
+    ctxGradient.beginPath();
+    ctxGradient.rect(platformX, c.height-platformHeight, platformWidth, platformHeight);
     ctxGradient.fillStyle = "#0095DD";
     ctxGradient.fill();
     ctxGradient.closePath();
@@ -21,12 +36,12 @@ function draw() {
         x = c.width/2;
         y = c.height-30;
     }
+
     ctxGradient.clearRect(0, 0, c.width, c.height);
     drawBall();
+    drawPlayer();
     x += dx;
     y += dy;
-    console.log(x);
-    console.log(y);
 }
 
 setInterval(draw, 10);
