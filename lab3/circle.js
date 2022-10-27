b84ea9444d6d4a10a1e9e17f667430547576e0f2
Bena2000 <specjunior@gmail.com>
2022-10-27 16:40:37 +0200

    moving ball

--- lab3/circle.js
+++ lab3/circle.js
@@ -1,15 +1,41 @@
 var c = document.getElementById("circle");
 var ctxGradient = c.getContext("2d");
-//shape code
-ctxGradient.beginPath();
-ctxGradient.arc(200, 150, 150, 0, 2 * Math.PI);
-ctxGradient.fillStyle = "blue";
-ctxGradient.fill();
-ctxGradient.closePath();
 
-setInterval(ChangeColorGradient, 1);
-var green=0;
-var changeToGreen=1;
+var x = c.width/2;
+var y = c.height-30;
+var dx = 0;
+var dy = -2;
+
+function drawBall() {
+    ctxGradient.beginPath();
+    ctxGradient.arc(x, y, 10, 0, Math.PI*2);
+    ctxGradient.fillStyle = "#0095DD";
+    ctxGradient.fill();
+    ctxGradient.closePath();
+}
+
+function draw() {
+    ctxGradient.clearRect(0, 0, c.width, c.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
+
+function drawGradientBall(){
+    //shape code
+    ctxGradient.beginPath();
+    ctxGradient.arc(200, 150, 150, 0, 2 * Math.PI);
+    ctxGradient.fillStyle = "blue";
+    ctxGradient.fill();
+    ctxGradient.closePath();
+    
+    setInterval(ChangeColorGradient, 1);
+    var green=0;
+    var changeToGreen=1;
+
+}
 
 function ChangeColorGradient(){
     if(changeToGreen===1)
